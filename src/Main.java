93c65897731f75195c28e65de788111b1da12622
NahomAl <128564728+NahomAl@users.noreply.github.com>
2023-10-04 16:38:35 -0800

    cleared hello world

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import Containers.LoginFrame;
-import UIComponents.*;
 
 public class Main {
     public static void main(String[] args) {
 //        new GFrame();
         new LoginFrame();
-        System.out.println("Hello world!");
     }
 }
